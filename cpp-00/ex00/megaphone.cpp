aa627f20774d122b2d7c81e3198b2398d69afda8
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2026-06-17 23:15:01 +0300

    Add files via upload

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -3,22 +3,25 @@
 
 int main(int ac, char *av[])
 {
+    int i;
+
     if (ac == 1)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
-    
-    int i   = 1;
+    i = 1;
     while (i < ac)
     {
-        int j =0;
+        int j;
+
+        j = 0;
         while(av[i][j]  != '\0')
         {
             std::cout << (char)std::toupper(av[i][j]);
             j++;
         }
-        if (i < ac - 1)
+        if(i < ac - 1)
             std::cout << " ";
         i++;
     }
