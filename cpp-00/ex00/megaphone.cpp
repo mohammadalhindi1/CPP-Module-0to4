8e985a0a4d9e6ec511b903664659f7991b0d1ee9
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2026-07-13 09:19:32 +0300

    Update megaphone.cpp

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <cctype>
 
-int main(int ac, char *av[])
+int main(int ac, char **av)
 {
     int i;
+    int j;
 
     if (ac == 1)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+                  << std::endl;
         return (0);
     }
     i = 1;
     while (i < ac)
     {
-        int j;
-
         j = 0;
-        while(av[i][j]  != '\0')
+        while (av[i][j] != '\0')
         {
-            std::cout << (char)std::toupper(av[i][j]);
+            std::cout << static_cast<char>(
+                std::toupper(static_cast<unsigned char>(av[i][j]))
+            );
             j++;
         }
-        if(i < ac - 1)
-            std::cout << " ";
         i++;
     }
     std::cout << std::endl;
