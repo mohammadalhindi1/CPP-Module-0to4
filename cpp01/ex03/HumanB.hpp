a3a0ad3bed9f0b60858bdbd3cd45a1533f3a5f70
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2026-07-17 02:05:39 +0300

    Add CPP01 ex03 solution

---
+++ cpp01/ex03/HumanB.hpp
@@ -0,0 +1,19 @@
+#ifndef HUMANB_HPP
+# define HUMANB_HPP
+
+# include "Weapon.hpp"
+# include <iostream>
+
+class HumanB
+{
+private:
+	std::string	_name;
+	Weapon		*_weapon;
+
+public:
+	HumanB(std::string name);
+	void	setWeapon(Weapon &weapon);
+	void	attack(void) const;
+};
+
+#endif
