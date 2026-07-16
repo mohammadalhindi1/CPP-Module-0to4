44d8585ddc22d98a056b6611055f247830e80b0f
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2026-07-17 02:05:16 +0300

    Add CPP01 ex01 solution

---
+++ cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,18 @@
+#include "Zombie.hpp"
+
+Zombie	*zombieHorde(int N, std::string name)
+{
+	Zombie	*horde;
+	int		i;
+
+	if (N <= 0)
+		return (NULL);
+	horde = new Zombie[N];
+	i = 0;
+	while (i < N)
+	{
+		horde[i].setName(name);
+		i++;
+	}
+	return (horde);
+}
